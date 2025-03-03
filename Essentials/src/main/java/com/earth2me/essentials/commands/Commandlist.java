b69fa184de8a151b6953ae41725ce9a3b8b984c1
RitzKid76 <tjbaker2003@hotmail.com>
2025-03-02 21:13:53 -0500

    added pages to the player list
    also cleaned up some garbage code since a lot of stuff was unnecessary

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandlist.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandlist.java
@@ -10,7 +10,8 @@ import org.bukkit.entity.Player;
 import java.util.*;
 
 public class Commandlist extends EssentialsCommand {
-    private final String showHiddenPlayers_PermissionNode = "essentials.list.hidden";
+    private final String SHOW_HIDDEN_PLAYERS_PERMISSION_NODE = "essentials.list.hidden";
+    private final int PLAYERS_PER_PAGE = 15;
 
     public Commandlist() {
         super("list");
@@ -23,31 +24,80 @@ public class Commandlist extends EssentialsCommand {
         String commandLabel,
         String[] args
     ) {
-        Player[] allPlayers = server.getOnlinePlayers();
-
-        List<Player> onlinePlayers = getOnlinePlayers(allPlayers);
-        int onlinePlayerCount = onlinePlayers.size();
-
-        List<Player> hiddenPlayers = getHiddenPlayers(allPlayers);
-        int hiddenPlayerCount = hiddenPlayers.size();
-
         boolean showHiddenPlayers = canListHiddenPlayers(sender);
-        if(showHiddenPlayers)
-            onlinePlayerCount += hiddenPlayerCount;
+        Player[] players = server.getOnlinePlayers();
 
-        String chatHeader = getCommandChatHeader(onlinePlayerCount, hiddenPlayerCount, showHiddenPlayers);
-        sender.sendMessage(chatHeader);
+        List<Player> sortedPlayers = new ArrayList<>();
 
-        addOnlinePlayers(sender, onlinePlayers);
-        if(showHiddenPlayers)
-            addHiddenPlayers(sender, hiddenPlayers);
+        int hiddenPlayerCount = 0;
+        if(showHiddenPlayers) {
+            List<Player> hiddenPlayers = getHiddenPlayers(players);
+
+            hiddenPlayerCount = hiddenPlayers.size();
+            sortedPlayers.addAll(hiddenPlayers);
+        }
+        sortedPlayers.addAll(getOnlinePlayers(players));
+
+        int playerCount = sortedPlayers.size();
+
+        int pageToView;
+        int pageCount;
+        try {
+            int[] pageDetails = getPageDetails(playerCount, args);
+            pageToView = pageDetails[0];
+            pageCount = pageDetails[1];
+        } catch (NumberFormatException e) {
+            String errorMessage = Util.i18n("pageIsNotAnInteger");
+            sender.sendMessage(errorMessage);
+
+            return;
+        }
+
+        sendChatMessage(sender, sortedPlayers, playerCount, hiddenPlayerCount, pageToView, pageCount);
     }
-    
-    private String getCommandChatHeader(
+
+    private void sendChatMessage(
+        CommandSender sender,
+        List<Player> players,
         int playerCount,
         int hiddenPlayerCount,
-        boolean showHiddenPlayers
+        int pageToView,
+        int pageCount
     ) {
+        String chatHeader = getChatHeader(playerCount, hiddenPlayerCount);
+        sender.sendMessage(chatHeader);
+
+        int startIndex = (pageToView - 1) * PLAYERS_PER_PAGE;
+
+        addPlayers(sender, players, startIndex);
+
+        String chatFooter = getChatFooter(pageToView, pageCount);
+        sender.sendMessage(chatFooter);
+    }
+
+    private int[] getPageDetails(int playerCount, String[] args) throws NumberFormatException {
+        int pagesAllowed = playerCount / PLAYERS_PER_PAGE + 1;
+        int[] output = new int[] {1, pagesAllowed};
+
+        if (args.length == 0)
+            return output;
+
+        String pageString = args[0];
+        int pageNumber = Integer.parseInt(pageString);
+
+        if (pageNumber < 1)
+            return output;
+
+        if (pageNumber > pagesAllowed) {
+            output[0] = pagesAllowed;
+            return output;
+        }
+
+        output[0] = pageNumber;
+        return output;
+    }
+
+    private String getChatHeader(int playerCount, int hiddenPlayerCount) {
         String output = "";
 
         if (playerCount == 1)
@@ -55,11 +105,15 @@ public class Commandlist extends EssentialsCommand {
         else
             output = Util.format("playersOnlineMultiple", playerCount);
 
-        if (showHiddenPlayers && hiddenPlayerCount > 0)
+        if (hiddenPlayerCount > 0)
             output += " " + Util.format("playersOnlineHiddenTag", hiddenPlayerCount);
 
         output += ":\n";
         return output;
+    }
+
+    private String getChatFooter(int page, int totalPages) {
+        return Util.format("pageNumberDisplay", page, totalPages);
     }
 
     private boolean canListHiddenPlayers(CommandSender sender) {
@@ -68,7 +122,7 @@ public class Commandlist extends EssentialsCommand {
             return true;
 
         User commandUser = ess.getUser(sender);
-        if (commandUser.isAuthorized(showHiddenPlayers_PermissionNode))
+        if (commandUser.isAuthorized(SHOW_HIDDEN_PLAYERS_PERMISSION_NODE))
             return true;
 
         return false;
@@ -80,51 +134,60 @@ public class Commandlist extends EssentialsCommand {
 
     private List<Player> getOnlinePlayers(Player[] allPlayers) {
         List<Player> output = new ArrayList<>();
-        
+
         for (Player player : allPlayers) {
-            if(isHiddenPlayer(player))
+            if (isHiddenPlayer(player))
                 continue;
 
             output.add(player);
         }
-    
+
         return output;
     }
 
     private List<Player> getHiddenPlayers(Player[] allPlayers) {
         List<Player> output = new ArrayList<>();
-        
+
         for (Player player : allPlayers) {
-            if(isHiddenPlayer(player))
+            if (isHiddenPlayer(player))
                 output.add(player);
         }
 
         return output;
     }
 
-    private String getPlayerString(User user, boolean hidden) {
+    private String getPlayerString(User user) {
         String output = "";
 
-        if(hidden)
+        if (user.isHidden())
             output = "§7[§dHIDDEN§7]";
-        if(user.isAfk())
+        if (user.isAfk())
             output += "§7[§8AFK§7]";
-        
+
         output += user.getDisplayName();
         return output;
     }
 
-    private void addOnlinePlayers(CommandSender sender, List<Player> players) {
-        addPlayers(sender, players, false);
-    }
-    private void addHiddenPlayers(CommandSender sender, List<Player> players) {
-        addPlayers(sender, players, true);
-    }
+    private int addPlayers(
+        CommandSender sender,
+        List<Player> players,
+        int startIndex
+    ) {
+        int playerCount = players.size();
 
-    private void addPlayers(CommandSender sender, List<Player> players, boolean hidden) {
-        if(players.size() == 0)
-            return;
-        
+        if (playerCount == 0)
+            return 0;
+
+        /**
+         * this looks complicated, but it just generates a segment of the player list starting at the 'startIndex'.
+         * it tries to get PLAYERS_PER_PAGE names, but till prevent IOOBE if there arent enough players present
+         */
+        int namesAbleToPrint = Integer.min(playerCount - startIndex, PLAYERS_PER_PAGE);
+        players = players.subList(
+            startIndex,
+            namesAbleToPrint + startIndex
+        );
+
         List<User> onlineUsers = new ArrayList<>();
         for (Player player : players) {
             User user = ess.getUser(player);
@@ -132,10 +195,12 @@ public class Commandlist extends EssentialsCommand {
         }
 
         for (User user : onlineUsers) {
-            String playerName = getPlayerString(user, hidden);
+            String playerName = getPlayerString(user);
             playerName += "\n";
 
             sender.sendMessage(playerName);
         }
+
+        return namesAbleToPrint;
     }
 }
